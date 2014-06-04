05dc048b2a77f18ee63a5220e70146882e221b61
kaligari <github.com@soften.pl>
2014-06-04 13:11:00 +0200

    Pierwsza wersja, prosto z ftp
    Signed-off-by: kaligari <github.com@soften.pl>

---
+++ index.php
@@ -0,0 +1,50 @@
+<html>
+<head> 
+<meta charset="UTF-8"/>
+<link rel="stylesheet" type="text/css" media="all" href="css/YTPlayer.css"/>
+<link rel="stylesheet" type="text/css" media="all" href="css/style.css"/>
+<link href='http://fonts.googleapis.com/css?family=Poiret+One&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+<script src="js/jquery.mb.YTPlayer.js" type="text/javascript"></script>
+<script src="js/script.js" type="text/javascript"></script>
+</head>
+<body>
+
+<a class="player" data-property="{videoURL:'54_3chytiVg',containment:'body',autoPlay:true, mute:true,opacity:0.5,showControls: false,quality:'hd720',startAt:5}">bgVideo</a>
+
+<div id="sztruks"></div>
+<div id="content">
+  <!-- NAV -->
+  <div id="menu">
+    <a href="#">ABOUT</a>
+    <a href="#">CONTACT</a>
+  </div>
+  <div id="arr-left">
+    <a href="#" class="scale-on-hover">            
+      <img src="img/left.png" alt="dalej" />
+    </a>
+  </div>
+  <div id="arr-right">          
+    <a href="#" class="scale-on-hover">            
+      <img src="img/right.png" alt="dalej" />
+    </a>
+  </div>
+  <!-- END NAV -->
+  <!-- SLIDES -->
+  <div class="page start" id="slide1">
+    <p>Regionalne Centrum Zdrowia w Zbąszynku</p>
+    <img src="portfolio/rcz.png">    
+  </div>
+  <div class="page" id="slide2">
+    <p>Remember!</p>
+    <img src="portfolio/rcz.png">    
+  </div>
+  <div class="page" id="slide3">
+    <p>Dont forget!</p>    
+  </div>
+  <!-- END SLIDES -->
+</div>
+</div>
+</body>
+</html>
